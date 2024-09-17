00a8319e1bf0f99a259f32da78b0263a7216b4c4
Lord Liberté <GhostRg110@gmail.com>
2024-09-17 23:47:42 +0200

    Update Lector_Voz_Documentos.py
    Se ha añadido una opción para modificar la velocidad del lector, para realizar pruebas de documentos sin esperar tanto. Se han añadido comentarios de aclaración del codigo

--- En desarrollo/Lector_Voz_Texto/Lector_Voz_Documentos.py
+++ En desarrollo/Lector_Voz_Texto/Lector_Voz_Documentos.py
@@ -1,10 +1,13 @@
 """Este documento trata de leer un archivo de texto y narrarlo con voz"""
 
+# IMPORTACIÓN DE LIBRERIAS ------------
 import pyttsx3
 import PyPDF2
 
+# ruta para el archivo a probar
 archivo = "C:\Repositorios GitHub\Curriculum-Carlos\En desarrollo\Lector_Voz_Texto\Informe.pdf"
 
+# abre el archivo PDF, lo lee, le encuentra el numero de paginas y crea una lista desde 0 hasta el numero de paginas (+1, por ser este numero excluyente)
 pdf_file = open(archivo, 'rb')
 pdf_reader = PyPDF2.PdfReader(pdf_file)
 paginas = len(pdf_reader.pages)
@@ -14,14 +17,18 @@ lista_paginas = list(range(0,paginas+1))
 def inicializar_voz(pagina):
     
     id1 = 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Speech\\Voices\\Tokens\\TTS_MS_ES-ES_HELENA_11.0'
+    # inicializa la voz del lector
     engine = pyttsx3.init()
     engine.setProperty('voices', id1)
-    newVoiceRate = 700
+    # Para pruebas, se ha añadido la velocidad de lectura
+    newVoiceRate = 400
     engine.setProperty('rate',newVoiceRate)
     
+    # inicia la lectura del input que recibe en pagina
     engine.say(pagina)
     engine.runAndWait()
 
+# en función del numero de páginas se crea un bucle equivalente a la longitud del archivo, realizando la lectura del texto en cada página
 for page in lista_paginas:
     page_obj = pdf_reader.pages[page]
     texto = page_obj.extract_text()
