f810cd6a862dee6f8f7575b1fe7f6037cafe132c
Lord Liberté <GhostRg110@gmail.com>
2024-09-29 10:58:13 +0200

    Update main.py

--- En desarrollo/Traductor_llamadas_tiempo_real/main.py
+++ En desarrollo/Traductor_llamadas_tiempo_real/main.py
@@ -1,7 +1,8 @@
 """Este documento es la linea general del proyecto. Se basa en un sistema que permite hablar y realizar una traducción directa de lo que habla"""
 
 import pyttsx3
+from dotenv import load_dotenv
+import os
 
-def escuchar():
-    engine = pyttsx3.engine
-    
+load_dotenv()
+api_key = os.getenv('API_KEY_DeepL')
