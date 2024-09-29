73558c01a0d8e1f433a4e7a8fe7fe80e8f0ea56d
Lord Liberté <GhostRg110@gmail.com>
2024-09-29 10:50:23 +0200

    Pruebas

---
+++ En desarrollo/Traductor_llamadas_tiempo_real/main.py
@@ -0,0 +1,7 @@
+"""Este documento es la linea general del proyecto. Se basa en un sistema que permite hablar y realizar una traducción directa de lo que habla"""
+
+import pyttsx3
+
+def escuchar():
+    engine = pyttsx3.engine
+    
